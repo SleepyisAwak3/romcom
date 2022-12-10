31c7626db05b63c1ff22d4341b5f2dafa338123a
danielcurtin <114776048+danielcurtin@users.noreply.github.com>
2022-12-09 19:53:56 -0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -17,7 +17,9 @@ var newCoverForm = document.querySelector(".view.form-view.hidden");
 
 var viewingSaved = false;
 
-var savedCovers = [new Cover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")];
+var savedCovers = [
+  new Cover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
+];
 var currentCover;
 
 
